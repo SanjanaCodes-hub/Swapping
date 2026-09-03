c95e3b02e6a5209128b729b6afab11f3731d4105
Sanjana Davagi <sanjanadavagi@gmail.com>
2026-09-03 12:54:33 +0300

    Initial Commit

---
+++ Swapping.py
@@ -0,0 +1,15 @@
+# Swapping of two numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Before swapping:")
+print("a =", a)
+print("b =", b)
+
+# Swap the numbers
+a, b = b, a
+
+print("After swapping:")
+print("a =", a)
+print("b =", b)
